daf4fee4fba444656dfda37de0cc7a705af806df
PrasannaKumar1234 <112098296+PrasannaKumar1234@users.noreply.github.com>
2022-09-02 10:54:34 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,56 @@
+
+
+// 1.  Write a program to find whether a given year is a leap year or not.
+
+//  function LeapYear(year){
+//     if(year%4===0){
+//         console.log('leap year')
+
+//     }
+//     else {                         
+//         console.log('not leap year')
+//     }
+// }
+
+// LeapYear(2004)
+
+//2.  Write a JavaScript program to convert temperatures to and from Celsius,Fahrenheit.
+
+// const cToF = function(celcius){
+//     const cTemp=celcius;
+//     const ToFahr=cTemp*(9/5)+32;
+//     const msg=`${cTemp}\xB0C is ${ToFahr} \xB0F.`;
+//     return msg;                                                 
+
+// }
+
+// const fToC = function(fahrenheit){
+//     const fTemp=fahrenheit;
+//     const fToCel=(fTemp-32)*(5/9);
+//     const msg=`${fTemp}\xB0F is ${fToCel} \xB0C.`;
+//     return msg;
+// }
+
+
+// console.log(cToF(60));
+
+// console.log(fToC(45));
+
+//3. Write a program to find the factorial of a number
+
+// function factorial(n){
+//     let answer = 1;
+//     if (n == 0 || n == 1){
+//       return answer;
+//     }else{
+//       for(var i = n; i >= 1; i--){
+//         answer = answer * i;               
+//       }
+//       return answer;
+//     }  
+//   }
+//   let n = 7;
+//   answer = factorial(n)
+//   console.log("The factorial of " + n + " is " + answer);
+
+
